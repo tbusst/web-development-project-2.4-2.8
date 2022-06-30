6396970d5d7c53923e963b3255545d0db103906c
Tom Busst <tombusstnz@gmail.com>
2022-07-01 11:11:44 +1200

    configured server to allow cors

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -13,7 +13,7 @@ export default function Home() {
     const [postsData, setPosts] = useState([]);
 
     useEffect(() => {
-        axios.get('/api/posts')
+        axios.get('https://web-development-project-server.herokuapp.com/api/posts')
             .then(res => res.data)
             .then(posts => {
                 setPosts(posts)
